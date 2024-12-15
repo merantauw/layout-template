268d8158e00a5876069ab43cd9c3c505e980f2d6
Merantauw <zholu.vlad2010@yandex.ru>
2024-12-16 00:15:08 +0500

    создал страницы каталог, продукт; адаптировал страницы блог, контакты

--- src/pages/product.js
+++ src/pages/product.js
@@ -2,9 +2,18 @@ import '../styles/common/normalize.scss';
 import '../styles/common/variables.scss';
 import '../styles/main.scss';
 import '../styles/common/product-page.scss';
+import '../styles/common/tabs-block.scss';
+import '../styles/common/form-section.scss';
+import '../styles/common/goods-section.scss';
+import '../styles/common/faq-section.scss';
 import '../styles/common/seo-section.scss';
 import '../styles/common/media.scss';
 
 import '../scripts/header.js';
 import '../scripts/product-page.js';
+import '../scripts/tabs-block.js';
+import '../scripts/goods-section.js';
+import '../scripts/faq-section.js';
+import '../scripts/seo-section.js';
+
 import '../scripts/smooth.js';
--- src/scripts/goods-section.js
+++ src/scripts/goods-section.js
@@ -1,25 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const goodsLists = document.querySelectorAll('.goods__list');
-    const scrollSpeed = 3;
-
-    goodsLists.forEach(goodsList => {
-        function handleWheel(event) {
-            const scrollLeft = goodsList.scrollLeft;
-            const scrollWidth = goodsList.scrollWidth;
-            const clientWidth = goodsList.clientWidth;
-
-            if (event.deltaY !== 0) {
-                if (event.deltaY > 0 && scrollLeft + clientWidth >= scrollWidth) {
-                    return;
-                } else if (event.deltaY < 0 && scrollLeft <= 0) {
-                    return;
-                }
-
-                event.preventDefault();
-                goodsList.scrollLeft += event.deltaY * scrollSpeed;
-            }
-        }
-
-        goodsList.addEventListener('wheel', handleWheel);
+    const swiper = new Swiper('.swiper-container', {
+        direction: 'horizontal',
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+        freeMode: true,
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            hide: true,
+        },
+        mousewheel: {
+            releaseOnEdges: true,
+            sensitivity: 1,
+        },
+        speed: 1700,
+        freeModeMomentum: true,
+        freeModeMomentumBounce: false,
     });
 });
